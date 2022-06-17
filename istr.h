5273c2ef3170a17e10c27e8375ce260bdd2462a7
Bruno Naibert <bruno@aurora>
2022-06-16 22:12:39 -0300

    Fix email on files copyright

--- istr.h
+++ istr.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Fabian Groffen
+ * Copyright 2020 Fabian Groffen <grobian@gentoo.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
