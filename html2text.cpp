83ed060b4a01c0fe6339833349416a7ae9ac718a
Fabian Groffen <grobian@gentoo.org>
2023-07-29 11:42:35 +0200

    *: update copyright year on files changed by previous commit
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- html2text.cpp
+++ html2text.cpp
@@ -5,6 +5,8 @@
  *
  * Author: Arno Unkrig <arno@unkrig.de>
  *
+ * Copyright 2020-2023 Fabian Groffen <grobian@gentoo.org>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
